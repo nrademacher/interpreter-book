9fb12554bb35e802ae4c43b106a6f6f104cc3184
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-11-13 23:16:13 +0100

    chore: progress (lexer)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # interpreter-book
 Follow-up along to Writing an Interpreter in Go by Thorsten Ball
+
+## TODO
+
+* Unicode support (currently ASCII only)
--- interpreter/token/token.go
+++ interpreter/token/token.go
@@ -32,3 +32,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// Check whether given identifier is keyword or user-defined identifier
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
